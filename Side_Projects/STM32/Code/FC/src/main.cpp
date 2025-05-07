9a581974f8c00eee7ff021bf8a6edf46eb8dfc4e
Drew Brandt <ahbrandt@umd.edu>
2025-05-06 20:50:48 -0400

    First pass at building with MMFS

--- Side_Projects/STM32/Code/FC/src/main.cpp
+++ Side_Projects/STM32/Code/FC/src/main.cpp
@@ -1,18 +1,37 @@
 #include <Arduino.h>
+#include <MMFS.h>
+#include "AvionicsState.h"
+#include "AviEventListener.h"
+#include "VoltageSensor.h"
 
-// put function declarations here:
-int myFunction(int, int);
+using namespace mmfs;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+MAX_M10S m;
+DPS310 d;
+BMI088andLIS3MDL b;
+VoltageSensor vsfc(A0, 330, 220, "Flight Computer Voltage");
+
+Sensor *s[] = {&m, &d, &b, &vsfc};
+AvionicsState t(s, sizeof(s) / 4, nullptr);
+
+MMFSConfig a = MMFSConfig()
+                   .withBBAsync(true, 50)
+                   .withBBPin(LED_BUILTIN)
+                   .withBBPin(32)
+                   //   .withBuzzerPin(33)
+                   .withUsingSensorBiasCorrection(true)
+                   .withUpdateRate(5)
+                   .withState(&t);
+MMFSSystem sys(&a);
+
+AviEventLister listener;
+
+void setup()
+{
+    sys.init();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+    sys.update();
 }
